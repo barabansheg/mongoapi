9655ddbec44c1649b1043273c0d8a27697ee9ae4
barabansheg <barabansheg>
2019-03-20 00:04:13 +0300

    insert and update added

--- lib/mongo_api.ex
+++ lib/mongo_api.ex
@@ -4,7 +4,7 @@ defmodule MongoApi do
 	import Supervisor.Spec
   
 	def start(_type, _args) do
-		db_url = Application.get_env(:link, :db_url)
+		db_url = Application.get_env(:mongoapi, :db_url)
 		children = [
 			worker(Mongo, [[name: :mongo, pool: DBConnection.Poolboy, url: db_url]])
 		] 
@@ -19,5 +19,28 @@ defmodule MongoApi do
 	def find(collection, query) do
 		Mongo.find(:mongo, collection, query, pool: DBConnection.Poolboy) |> Enum.to_list
 	end
+
+	def insert(collection, document) do
+		Mongo.insert_one(:mongo, collection, document, pool: DBConnection.Poolboy)
+	end
+
+	def insert(collection, document, :result) do
+		{:ok, %{inserted_id: inserted_id}} = insert(collection, document)
+		findOne(collection, %{ _id: inserted_id })
+	end
+
+	def update(collection, query, document, :one) do
+		Mongo.update_one(:mongo, collection, query, %{"$set": document}, pool: DBConnection.Poolboy)
+	end
+
+	def update(collection, query, document) do
+		update(collection, query, document, :one)
+	end
+
+	def update(collection, query, document, :many) do
+		Mongo.update_many(:mongo, collection, query, %{"$set": document}, pool: DBConnection.Poolboy)
+	end
+
 	
+
   end
--- mix.exs
+++ mix.exs
@@ -14,15 +14,16 @@ defmodule MongoApi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {MongoApi, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mongodb, "== 0.4.6"},
+      {:poolboy, ">= 0.0.0"},
     ]
   end
 end
