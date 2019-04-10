e8a78f4476e7df351d34800b830e015a1af23f20
barabansheg <barabansheg@gmail.com>
2019-04-10 19:02:03 +0300

    change elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MongoApi.MixProject do
     [
       app: :mongoapi,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
